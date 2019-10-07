d7c6f8fbd245b6ffc907126854f14c98eee549a6
Alice Brunel <alc.brunel@gmail.com>
2019-10-07 17:03:48 +0000

    Defined signin root and how to create a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,14 @@ class UserController < ApplicationController
   end
 
   get '/signin' do
-
+    if params[:username] == "" || params[:email] == "" || params[:password] == "" || params[:role] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password], :role => params[:role])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    end
   end
 
 end
