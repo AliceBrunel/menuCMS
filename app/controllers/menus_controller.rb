e8721fec76c2eb13e05cbfd98ab82ec2633c7579
Alice Brunel <alc.brunel@gmail.com>
2019-10-07 16:41:37 +0000

    Created first route to sign up and create users, with assocciated views

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,3 +1,12 @@
 class MenusController < ApplicationController
 
+  get '/menus' do
+    if logged_in?
+      @menus = Menu.all
+      erb :'menus/menus'
+    else
+      redirect to '/signin'
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UserController < ApplicationController
 
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      redirect to '/menus'
+    end
+  end
+
+  get '/signin' do
+
+  end
+
 end
