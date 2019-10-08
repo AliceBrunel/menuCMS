0cf174a0b6b15184c9b94e0127303586e89949ee
Alice Brunel <alc.brunel@gmail.com>
2019-10-08 10:07:47 +0000

    Dropped DB and redo migration to change column password_digetst in users

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2019_10_07_151622) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "role"
   end
 
