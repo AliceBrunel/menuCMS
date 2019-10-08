b5384ca2c97cdb011aa306fae25faf5e3c44c1bd
Alice Brunel <alc.brunel@gmail.com>
2019-10-08 12:53:07 +0000

    Added an edit form for menu and patch in menus_controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -9,13 +9,13 @@ class MenusController < ApplicationController
     end
   end
 
-    get '/menus/create-menu' do
-      if logged_in?
-        erb :'menus/create_menu'
-      else
-        redirect to '/signin'
-      end
+  get '/menus/create-menu' do
+    if logged_in?
+      erb :'menus/create_menu'
+    else
+      redirect to '/signin'
     end
+  end
 
   get '/menus/:slug' do
     if logged_in?
@@ -25,7 +25,6 @@ class MenusController < ApplicationController
       redirect to '/signin'
     end
   end
-
 
   post '/menus' do
     if logged_in?
@@ -40,4 +39,39 @@ class MenusController < ApplicationController
       redirect to '/signin'
     end
   end
+
+  get '/menus/:slug/edit' do
+    if logged_in?
+    @menu = Menu.find_by_slug(params[:slug])
+      if @menu && @menu.user == current_user
+        erb :'menu/edit'
+      else
+        redirect to '/menus'
+      end
+    else
+      redirect to '/signin'
+    end
+  end
+
+#Patch for partial update of menu
+  patch '/menus/:slug' do
+    if logged_in?
+      if params[:name] == ""
+        redirect to "/menus/#{@menu.slug}/edit"
+      else
+        @menu = Menu.find_by_slug(params[:slug])
+          if @menu && @menu.user == current_user
+            @menu.update(params[:menu])
+            @menu.meal_ids = params[:meals]
+            @menu.save
+            redirect to "/menus/#{@menu.slug}"
+          else
+            redirect to "/menus/#{@menu.slug}/edit"
+          end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
 end
