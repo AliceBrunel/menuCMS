951bf040dfca5e40e449412d030bbc4dd7c9e5ae
Alice Brunel <alc.brunel@gmail.com>
2019-10-08 12:16:03 +0000

    Completed users_controller with the full sign up, log in, log out scenario, redo homepage to dynamically display log options

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,30 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/menus'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/menus'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/'
+    else
+      redirect to '/'
+    end
+
 end
