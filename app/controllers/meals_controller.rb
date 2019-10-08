17dac10881ce09dbacfafece24993947fbefee12
Alice Brunel <alc.brunel@gmail.com>
2019-10-08 17:54:21 +0000

    Created route to show all meals and corresponding erb template

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,3 +1,12 @@
 class MealsController < ApplicationController
 
+  get '/meals' do
+    if logged_in?
+      @meals = Meal.all
+      erb :'meals/meals'
+    else
+      redirect to '/signin'
+    end
+  end
+
 end
