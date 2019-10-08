117eebb43080132668f5f6c5afff73f839aa71ae
Alice Brunel <alc.brunel@gmail.com>
2019-10-08 10:09:46 +0000

    Corrected /menus route after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password], :role => params[:role])
       @user.save
       session[:user_id] = @user.id
-      redirect to '/tweets'
+      redirect to '/menus'
     end
   end
 
